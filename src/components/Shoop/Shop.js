cc3c5087a3640a517ba8291abd8646790ef63eda
Masum Hossain <masumhossain.dev@gmail.com>
2023-05-06 00:29:44 +0600

    some comp added

---
+++ src/components/Shoop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        <h3>This is for products</h3>
+      </div>
+      <div className="cart-container">
+        <h3>This is for cart</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
