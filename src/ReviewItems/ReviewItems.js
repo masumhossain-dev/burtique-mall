0e5f2e183d90ce68be93a1783c3703b877956329
Masum Hossain <masumhossain.dev@gmail.com>
2023-06-22 19:54:03 +0600

    whole project done

--- src/ReviewItems/ReviewItems.js
+++ src/ReviewItems/ReviewItems.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './ReviewItems.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
-const ReviewItems = ({product}) => {
+const ReviewItems = ({id, product, removeCartItems}) => {
+  const {img, name, price, quantity} = product;
   return (
-    <div>
-      <p>This is review items</p>
+    <div className='review-item'>
+      <div className='product-img'>
+        <img src={img} alt="" />
+      </div>
+      <div className="review-details-container">
+        <div className="review-details">
+          <p>{name}</p>
+          <p><small>Price: <span>{price}</span></small></p>
+          <p><small>Quantity: {quantity}</small></p>
+        </div>
+        <div className="delete-btn">
+          <button className='item-delete' onClick={()=>removeCartItems(id)}>
+            <FontAwesomeIcon icon={faTrashAlt} className='fa-trash'></FontAwesomeIcon>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,15 +2,27 @@ import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import ReviewItems from '../../ReviewItems/ReviewItems';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-  const {products, initialCart} = useLoaderData();
+  const {initialCart} = useLoaderData();
   const [cart, setCart] = useState(initialCart);
+
+  const removeCartItems = (id)=>{
+    const remaining = cart.filter(product=> product.id !== id);
+    setCart(remaining);
+    removeFromDb(id);
+  }
   return (
     <div className='shop-container'>
       <div className='order-container'>
       {
-          cart.map(product=><ReviewItems key={product.id} product={product}></ReviewItems>)
+          cart.map(product=><ReviewItems
+            key={product.id}
+            product={product}
+            id={product.id}
+            removeCartItems={removeCartItems}
+            ></ReviewItems>)
         }
       </div>
       <div className='cart-container'>
